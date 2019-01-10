4c8b1a634e134801fda7e02a8cac10aff1641236
beini19 <beinifang19@gmail.com>
2019-01-10 15:31:11 -0500

    setting up react

---
+++ client/src/components/ImagePreviewTable.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+// This is a component
+class ImagePreviewTable extends Component {
+// add constructor and render functions
+render() {
+    return (
+        // put jsx/html syntax here
+        <div>
+            <h1> asdfjkg </h1>
+            <table>
+                <tr>
+                    <td>asdf</td>
+                    <td>
+                        <a href='https://arizonaatwork.com'><img style={{display: 'block'}} width="100%" height="100%" src='https://i.imgur.com/fe0T4nw.png' alt=""/></a>
+                    </td>
+                    <td>asdf2</td>
+                </tr>
+            </table>
+        </div>
+    );
+}
+
+}
+
+export default ImagePreviewTable;
