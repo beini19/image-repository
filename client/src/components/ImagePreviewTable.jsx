d2684f9bd97019e400a4793cb7dee76dc2e66eb5
Ding Bei Ni Fang <dbnfang@uwaterloo.ca>
2019-01-15 23:41:26 -0500

    test rendering component inside loop

--- client/src/components/ImagePreviewTable.jsx
+++ client/src/components/ImagePreviewTable.jsx
@@ -27,28 +27,52 @@ class ImagePreviewTable extends Component {
       .catch(err => console.log(err));
   }
 
+  renderGrid() {
+    var row = [];
+    var table = [];
+    for (var i = 0; i < 20; i++) {
+        console.log(this.state.imageData);
+        var data = this.state.imageData[i];
+        var image = <div className="col-md-2" data-toggle="modal" data-target="#exampleModal" image-id={data.id} user-id={data.user_id} 
+                        onClick={() => {this.props.setUserData(data.user_id); this.props.setImageData(data.id)}}>
+                    {               
+                    <Image
+                    src={data.picture}              
+                    />
+                    }
+                    </div>;
+        if (i % 4 === 0) {
+            table.push(row);
+            row = [];
+        }
+        row.push(<div className="row">{image}</div>);
+    }
+    return table;
+  }
+
   render() {
     console.log(this.props)
+
+    
     
     return (
       <div>
         <h1> Content-Hub </h1>
         <div className="col-md-2"/>
-        {/* <div className="col-md-10"> */}
+        {this.renderGrid()}
+        {/* <div className="col-md-10">
           {// note: map function can only be used on lists
-            this.state.imageData.map((data) => (
-              <div className="col-md-2" data-toggle="modal" data-target="#exampleModal" image-id={data.id} user-id={data.user_id} 
-                    onClick={() => {this.props.setUserData(data.user_id); this.props.setImageData(data.id)}}>
-                {               
-                  <Image
-                  src={data.picture}
-                  image_id={data.id}
-                  user_id={data.user_id}                 
-                  />
-                }
-              </div>
-          ))}
-        {/* </div> */}
+        //     this.state.imageData.map((data) => (
+        //       <div className="col-md-2" data-toggle="modal" data-target="#exampleModal" image-id={data.id} user-id={data.user_id} 
+        //             onClick={() => {this.props.setUserData(data.user_id); this.props.setImageData(data.id)}}>
+        //         {               
+        //           <Image
+        //           src={data.picture}              
+        //           />
+        //         }
+        //       </div>
+        //   ))}
+        // </div> */}
         <div className="col-md-2"/>
       </div>
     );
